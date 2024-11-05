1469223036c12b3c41c316d95d54446215a4cc86
fredrik-p <fredrik.pekkarinen@gmail.com>
2024-11-05 19:13:02 +0100

    framer motion added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,35 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
-import "./globals.css";
+import InteractiveBackground from './interactive-background';
+import './globals.css';
 
-const outfit = Outfit({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'pekkarinen.dev',
   description: 'Personal website of Fredrik Pekkarinen',
-  metadataBase: new URL('https://hagelstudio-nqf91z4d6-fredrik-ps-projects.vercel.app/'),
+  metadataBase: new URL(
+    'https://hagelstudio-nqf91z4d6-fredrik-ps-projects.vercel.app/',
+  ),
   openGraph: {
     title: 'pekkarinen.dev',
     description: 'Personal website of Fredrik Pekkarinen',
     url: 'https://pekkarinen.dev',
-    images:
-    [
+    images: [
       {
         url: 'https://hagelstudio.com/opengraph-image.png',
-      }, 
+      },
     ],
     locale: 'sv_SE',
     type: 'website',
-  },  
-  keywords: ['Pekkarinen','Frontend','Developer','Fredrik', 'Fredrik Pekkarinen'],
+  },
+  keywords: [
+    'Pekkarinen',
+    'Frontend',
+    'Developer',
+    'Fredrik',
+    'Fredrik Pekkarinen',
+  ],
   publisher: 'Fredrik Pekkarinen',
 };
 
@@ -32,7 +40,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="sv">
-      <body className={outfit.className}>{children}</body>
+      <body className={outfit.className}>
+        <InteractiveBackground />
+        {children}
+      </body>
     </html>
   );
 }
